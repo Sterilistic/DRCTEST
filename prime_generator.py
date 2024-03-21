ee93ed7d0f64aaba201b8043a068f06483848c52
Pushpendra Kumar Tiwari <pushpendra.krtiwari@gmail.com>
2024-03-21 14:17:38 -0700

    adding comments and invalid range check

--- prime_generator.py
+++ prime_generator.py
@@ -18,11 +18,21 @@ def prime_generator(start, end):
 
 
 if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print("Usage: python prime_generator.py <start> <end>")
+        sys.exit(1)
     
     start = int(sys.argv[1])
     end = int(sys.argv[2])
+
+    if start < 0 or end < 0:
+        print("Invalid range. Start and end must be positive integers.")
+        sys.exit(1)
     
 
     primes = prime_generator(start, end)
 
-    print(primes)
+    if start > end:
+        start, end = end, start
+    print("Prime numbers between {} and {}: {}".format(start, end, primes))
